6c6eea0d234a1dc2f670091b6208178e0fd4e5f5
Cedric Leroy <cedie73@hotmail.fr>
2018-03-17 11:08:29 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+
+HERE = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(HERE, 'README.rst')) as f:
+    README = f.read()
+
+
+setup(
+    name='pyungo',
+    version='0.1.0',
+    description='Function dependencies resolution and execution',
+    long_description=README,
+    url='https://github.com/cedricleroy/pyungo',
+    author='Cedric Leroy',
+    author_email='cedie73@hotmail.fr',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='dag workflow function dependency',
+    packages=['pyungo'],
+)
