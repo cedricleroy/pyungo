0cbff2c9f504f95ec5dd773b6dad2bd37227781e
Cedric Leroy <cedie73@hotmail.fr>
2020-06-13 15:42:38 -0500

    Update Python versions compatibility

--- setup.py
+++ setup.py
@@ -7,35 +7,29 @@ from pyungo import __version__
 HERE = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(HERE, 'README.rst')) as f:
+with open(path.join(HERE, "README.rst")) as f:
     README = f.read()
 
 
 setup(
-    name='pyungo',
+    name="pyungo",
     version=__version__,
-    description='Function dependencies resolution and execution',
+    description="Function dependencies resolution and execution",
     long_description=README,
-    url='https://github.com/cedricleroy/pyungo',
-    author='Cedric Leroy',
-    author_email='cedie73@hotmail.fr',
-    license='MIT',
-    extra_require={
-        'all': [
-            'multiprocess',
-            'pycontracts',
-            'jsonschema'
-        ]
-    },
+    url="https://github.com/cedricleroy/pyungo",
+    author="Cedric Leroy",
+    author_email="cedie73@hotmail.fr",
+    license="MIT",
+    extra_require={"all": ["multiprocess", "pycontracts", "jsonschema"]},
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='dag workflow function dependency',
-    packages=['pyungo'],
+    keywords="dag workflow function dependency",
+    packages=["pyungo"],
 )
